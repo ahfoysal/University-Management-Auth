50bcbb9da5985f7ca56ae821f522a5935c9da213
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-07-17 16:48:11 +0600

    b

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-// import { User } from "./modules/user/user.model";
 
 const app: Application = express()
 
@@ -12,11 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
-  //insert a data
-
   res.send('Server Running')
-  // from new branch
-  // next()
 })
 
 export default app
