9909831fd29fe57fbcc1228fa8cf867891fc9a2b
hosan30 <50867996+hosan30@users.noreply.github.com>
2023-07-17 16:28:16 +0600

    A

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
-import express, { Application, NextFunction, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
 // import { User } from "./modules/user/user.model";
 
-const app: Application = express();
+const app: Application = express()
 
 ///cors
-app.use(cors());
+app.use(cors())
 
 ///body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
+app.get('/', (req: Request, res: Response) => {
   //insert a data
- 
-  res.send("Server Running");
+
+  res.send('Server Running')
   // from new branch
   // next()
-});
+})
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,18 @@
-import mongoose from 'mongoose';
-import app from './app';
-import config from './config';
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 //////Database Connection
-async function connectDB () {
-    try {
-        await mongoose.connect(config.database_url as string)
-        console.log("MongoDB Connected")
+async function connectDB() {
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('MongoDB Connected')
 
-        app.listen(config.port, () => {
-            console.log(`Server listening on port ${config.port}`)
-          })
-          
-        
-    }catch(err) {
-        console.log( 'failed to connect database',err)
-            }
-
-    }
-    connectDB()
-
+    app.listen(config.port, () => {
+      console.log(`Server listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('failed to connect database', err)
+  }
+}
+connectDB()
